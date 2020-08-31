f646c94f3e0729305e2eb5866e5abfb1dcde44dd
Jamie Mullan <jamiemullan@mac.local>
2020-08-31 20:38:40 +0100

    aRecord change

--- src/DNS/aRecord.ts
+++ src/DNS/aRecord.ts
@@ -2,12 +2,14 @@
 import { Construct } from 'constructs';
 import { Route53Record } from "../../.gen/providers/aws/route53-record";
 import { Route53Zone } from "../../.gen/providers/aws/route53-zone";
+import { S3Bucket } from '../../.gen/providers/aws';
 
 export class SiteARecord extends Route53Record {
-  constructor(scope: Construct, domain: string) {
-    super(scope, "myCert", {
-       name: "site",
-       type: "site",
+  constructor(scope: Construct, domain: string, bucket: S3Bucket) {
+    super(scope, "myRecord", {
+       name: domain,
+       records: [bucket.bucketDomainName],
+       type: "A",
        zoneId: new Route53Zone(scope, "lookup", {
            name: domain
        }).zoneId
